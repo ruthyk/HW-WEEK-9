8962bf62eeea8a9a7e3d30e5b9b23665ad6e3c5a
ruthyk <79362935+ruthyk@users.noreply.github.com>
2021-05-01 18:16:56 +1000

    index js additions and added questions

---
+++ index.js
@@ -0,0 +1,86 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'What is the license you are using for the repository?',
+      choices: ["MIT License", "Apache License"]
+    },
+    {
+      type: 'input',
+      name: 'installation ',
+      message: 'Please give instructions of how to install your application',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please explain how to use your application',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What is the code of conduct to be included in the contribution section?',
+        choices: ["Contributor Covenant", "Citizen Code Of Conduct"]
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide any details about running tests on your application',
+    },
+  ]);
+};
+
+const generateHTML = (answers) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+
+// Bonus using writeFileAsync as a promise
+const init = () => {
+  promptUser()
+    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
+};
+
+init();
