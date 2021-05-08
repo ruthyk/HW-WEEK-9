1e1200b77e89a85cd02bf6579ae8db9076e8caed
ruthyk <79362935+ruthyk@users.noreply.github.com>
2021-05-08 14:51:55 +1000

    added changes

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# HW-WEEK-9
+
+        # t
+          ![license](https://img.shields.io/badge/license-Apache License-blue.svg)
+          
+        Apache License
+        
+        ## Description
+        t  
+
+        ## Table of Contents
+        [Installation](#installation)  
+        [Usage](#usage)  
+        [License] (#license)
+        
+* [License](#license) 
+
+        [Contribution](#contribution)  
+        [Tests] (#test)
+        [Questions] (#questions)
+        
+        ## Installation
+        undefined  
+
+        ## Usage
+        t
+        undefined  
+
+        ## Contribution
+
+        Citizen Code Of Conduct
+
+        ## License
+        undefined
+
+        ## Running Tests
+        undefined
+
+        ## Questions
+        Github: t | Email: t;
--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const promptUser = () => {
         message: 'Enter your GitHub profile link',
       },
     {
-      type: 'input',
+      type: 'list',
       name: 'license',
       message: 'What is the license you are using for the repository?',
       choices: ["MIT License", "Apache License"]
@@ -43,7 +43,7 @@ const promptUser = () => {
       message: 'Please explain how to use your application',
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'contribution',
         message: 'What is the code of conduct to be included in the contribution section?',
         choices: ["Contributor Covenant", "Citizen Code Of Conduct"]
@@ -55,87 +55,116 @@ const promptUser = () => {
     },
   ]);
 
-      var descriptionoflicense;
-      var licensebadge;
-      var descriptionofcontribution; 
-      switch (license) {
-          case "MIT License":
-            licensebadge = "![license](https://img.shields.io/badge/license-MIT-green.svg)"
-            descriptionoflicense = 
-            "MIT license
-Copyright (c)
+}
+function renderLicenseBadge(license) {
+  let licensebadge = `![license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  return licensebadge;
+};
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE."
-            break;
-        case "Apache License 2.0":
-            licensebadge = "![license](https://img.shields.io/badge/license-Apache-blue.svg)"
-            descriptionoflicense = 
-            "Licensed under the Apache License, Version 2.0 (the "License");
-            you may not use this file except in compliance with the License.
-            You may obtain a copy of the License at
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  let licenseLink = `\n* [License](#license) \n`;
+  return licenseLink;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  let licenseSection = `${license}
+  Copyright (c)
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
+  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.`
+}
+
+
+//var descriptionoflicense;
+// var licensebadge;
+// var descriptionofcontribution; 
+// switch (license) {
+//     case "MIT License":
+//       licensebadge = "![license](https://img.shields.io/badge/license-MIT-green.svg)"
+//       descriptionoflicense = 
+//       `MIT license
+// Copyright (c)
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
+
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.`
+//             break;
+//         case "Apache License 2.0":
+//             licensebadge = "![license](https://img.shields.io/badge/license-Apache-blue.svg)"
+//             descriptionoflicense = 
+//             `Licensed under the Apache License, Version 2.0 (the "License");
+//             you may not use this file except in compliance with the License.
+//             You may obtain a copy of the License at
             
-                http://www.apache.org/licenses/LICENSE-2.0
+//                 http://www.apache.org/licenses/LICENSE-2.0
             
-            Unless required by applicable law or agreed to in writing, software
-            distributed under the License is distributed on an "AS IS" BASIS,
-            WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-            See the License for the specific language governing permissions and
-            limitations under the License."
-            break; 
-        }
-        const generateREADME = (answers) =>
-        # ${title}
-        ${license}
+//             Unless required by applicable law or agreed to in writing, software
+//             distributed under the License is distributed on an "AS IS" BASIS,
+//             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//             See the License for the specific language governing permissions and
+//             limitations under the License.`
+//             break; 
+//         }
+
+const generateReadme = (answers) => {
+  return `
+        # ${answers.title}
+          ${renderLicenseBadge(answers.license)}
+          
+        ${answers.license}
+        
         ## Description
-        ${usage}  
+        ${answers.usage}  
 
         ## Table of Contents
         [Installation](#installation)  
         [Usage](#usage)  
         [License] (#license)
+        ${renderLicenseLink(answers.license)}
         [Contribution](#contribution)  
         [Tests] (#test)
         [Questions] (#questions)
         
         ## Installation
-        ${installation}  
+        ${answers.installation}  
 
         ## Usage
-        ${usage}  
+        ${answers.usage}
+        ${renderLicenseSection(answers.license)}  
 
         ## Contribution
 
-        ${contribution}
+        ${answers.contribution}
 
         ## License
-        ${descriptionoflicense}
+        ${answers.descriptionoflicense}
 
         ## Running Tests
-        ${tests}
+        ${answers.tests}
 
         ## Questions
-        Github: ${github} | Email: ${email};
+        Github: ${answers.github} | Email: ${answers.email};`
     }
 
 // Bonus using writeFileAsync as a promise
 const init = () => {
   promptUser()
-    .then((answers) => writeFileAsync('readme.md', generatereadme(answers)))
+    .then((answers) => writeFileAsync('readme.md', generateReadme(answers)))
     .then(() => console.log('Successfully wrote to readme.md'))
     .catch((err) => console.error(err));
 };
